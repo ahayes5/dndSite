a9705ecb3d50cec36fb7136f44f51877958bcad2
ahayes <hayesandy5@gmail.com>
2022-11-15 07:45:12 -0500

    initial

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <nav>
+        
+      </nav>
+    </div>
+  </header>
+
+  <v-app>
+    <v-app-bar absolute dense elevation="5" color="white" hide-on-scroll>
+        <v-btn><RouterLink to="/" class="navitem">Home</RouterLink></v-btn>
+        <v-btn><RouterLink to="/about" class="navitem">About</RouterLink></v-btn>
+        <v-btn><a href="/foundry" class="navitem">Foundry</a></v-btn>
+    </v-app-bar>
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const app = createApp(App)
+
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: false,
+  })
+
+
+app.use(router)
+app.use(vuetify)
+app.mount('#app')
